c7eed0e79f6e398910600588c65b20b6bc4c3611
Steven Hunal <stevehunal@gmail.com>
2019-09-18 20:50:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,19 @@ class App < Sinatra::Base
  
  get "/square/:number" do
    
- end 
+ end
+ 
+ get "/say/:number/:phrase" do
+   
+ end
+ 
+ get "/say/:word1/:word2/:word3/:word4/:word5" do
+   
+ end
+ 
+ get "/:operation/:number1/:number2" do
+   
+ end
  
  
  
